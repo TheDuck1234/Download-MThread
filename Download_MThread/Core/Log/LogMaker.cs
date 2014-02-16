5b0fcc6b3d4688e499e0c44f819b59ab8fb90107
Anders Petersen <Jinx.petersen@gmail.com>
2014-02-16 16:31:06 +0100

    Update with log

---
+++ Download_MThread/Core/Log/LogMaker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExcelNumberCleanser.Core.Log
+{
+	public static class LogMaker
+	{
+		public static void MakeListLog(List<Log> linesList, string path )
+		{
+			var text = linesList.Select(log => log.GiveLog()).ToList();
+			File.WriteAllLines(path, text);
+		}
+
+		public static string[] ReadLogs(string folderPath)
+		{
+			var pdfFiles = Directory.GetFiles(folderPath, "*.txt")
+									 .Select(Path.GetFileName)
+									 .ToArray();
+
+			return pdfFiles;
+		}
+	}
+}
---
+++ Download_MThread/Core/Log/LogManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExcelNumberCleanser.Core.Log
+{
+	public class Log
+	{
+		public string Data { get; set; }
+		public string Time = DateTime.Now.ToLongTimeString();
+
+		public string GiveLog()
+		{
+			return Time + " | " + Data;
+		}
+	}
+	public class LogManager
+	{
+		public List<Log> LogList { get; private set; }
+
+		public LogManager()
+		{
+			LogList = new List<Log>();
+		}
+
+		public void Log(string text)
+		{
+			LogList.Add(new Log{Data = text});
+		}
+
+		public void FinishLog()
+		{
+			var path = AppSettings.GetLogPath()+DateTime.Now.ToShortDateString() +".txt";
+			LogMaker.MakeListLog(LogList,path);
+		}
+	}
+}
