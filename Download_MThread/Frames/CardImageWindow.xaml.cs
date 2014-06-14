7eca8c57f1196bb3be61433709a9e7ffc70b5233
Anders Petersen <Jinx.petersen@gmail.com>
2014-06-14 22:13:27 +0200

    NewWindows

---
+++ Download_MThread/Frames/CardImageWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Download_MThread.Frames
+{
+    /// <summary>
+    /// Interaction logic for CardImageWindow.xaml
+    /// </summary>
+    public partial class CardImageWindow : Window
+    {
+        public CardImageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
