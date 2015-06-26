86fa6b4cf68c52dd136631f7d8a4b6fc842366ab
Anders Petersen <jinx.petersen@gmail.com>
2015-06-26 14:25:53 +0200

    Revert "Test"

--- Download_MThread/MainWindow.xaml.cs
+++ Download_MThread/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using Download_MThread.Core;
 using System;
 using System.Collections.Generic;
@@ -9,31 +10,49 @@ using System.Windows;
 using System.Windows.Threading;
 using Download_MThread.Core.Download;
 using Download_MThread.Core.Log;
+using Download_MThread.Frames;
 
 namespace Download_MThread
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         private int _count;
         private DateTime _starttime;
-        private readonly string _xmlFileName = AppSetting.GetXmlFileName();
+        private DispatcherTimer _timer;
+        private ImageFrame _imageFrame;
+
+        // does not work yet
+        private bool _running = false;
+        private readonly CancellationTokenSource _tokenSource = new CancellationTokenSource();
+        //
 
 
         public MainWindow()
         {
             InitializeComponent();
+            SetupListBox();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var testlist = XmlReader.GetXmlFiles(_xmlFileName);
-            var lists = DownloadLoader.Partition(testlist, 5);
-            // Create and collect tasks in list
-            _starttime = DateTime.Now;
+            //var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            //var token = tokenSource.Token;
+            //if (_running)
+            //{
+            //    _tokenSource.Cancel();
+            //    _running = false;
+            //    return;
+            //}
+            //running = true;
+
             ToggleButton(false);
+            var testlist = XmlReader.GetXmlFiles(AppSettings.GetXmlFileName());
+            var lists = DownloadLoader.Partition(testlist, 5);
+            SetupTimer();
+            // Create and collect tasks in list
+
+            _starttime = DateTime.Now;
+
             var tasks = lists.Select(list => Task.Factory.StartNew(() =>
             {
                 var worker = new DownloadWorker();
@@ -44,12 +63,14 @@ namespace Download_MThread
                         _count++;
                     }
                     Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate
-                    {
-                        // ReSharper disable once RedundantCast
+                    {    
+
                         var procent = ((100 * _count) / testlist.Count);
                         ProgressBar.Value = procent;
                         ProgressLabel.Content = procent + "%";
+
                         //CountLabel.Content = "Total items cleanse: " + _count;
+
                         if (_count > 0)
                         {
                             EstimateTimeLabel.Content = "Time: " + EstimateTime(testlist.Count).ToString();
@@ -57,7 +78,7 @@ namespace Download_MThread
                     });
                 };
 
-                var path = Directory.GetCurrentDirectory() + @"\Card Data";
+                var path = Directory.GetCurrentDirectory() + AppSettings.GetImagePath();
 
                 if (!Directory.Exists(path))
                 {
@@ -67,24 +88,27 @@ namespace Download_MThread
                 return result;
 
 
-            })).ToList();
-            // ReSharper disable once ImplicitlyCapturedClosure
+            }, _tokenSource.Token)).ToList();
+            
             Task.Factory.StartNew(() =>
             {
                 var results = new List<Log>();
+
                 // Wait till all tasks completed
 
                 foreach (var result in tasks.Select(task => task.Result))
                 {
                     results.AddRange(result.ToList());
                 }
-                var path = Directory.GetCurrentDirectory() + @"\Logs";
+                var path = Directory.GetCurrentDirectory() + AppSettings.GetLogPath();
+
+                _timer.Stop();
 
                 LogMaker.MakeListLog(results, path);
 
-                //ToggleButton(true);
-            });
-            ToggleButton(true);
+                
+            }, _tokenSource.Token);
+            SetupListBox();
         }
         private TimeSpan EstimateTime(int max)
         {
@@ -97,15 +121,101 @@ namespace Download_MThread
         private void ToggleButton(bool status)
         {
             TestButton.IsEnabled = status;
-            DcButton.IsEnabled = status;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            var path = Directory.GetCurrentDirectory() + @"\Card Data";
 
-            DownloadLoader.DeleteAllCache(path);
-            MessageBox.Show("Caches Deleted");
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var path = Directory.GetCurrentDirectory() + AppSettings.GetImagePath();
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            Process.Start(path);
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            var path = Directory.GetCurrentDirectory() + AppSettings.GetImagePath();
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            var delete = DownloadLoader.DeleteAllCache(path);
+            ProgressLabel.Content = "";
+            EstimateTimeLabel.Content = "";
+            ProgressBar.Value = 0;
+            SetupListBox();
+            ToggleButton(true);
+            MessageBox.Show(delete ? "Caches deleted" : "No caches to deleted");
+        }
+
+        private void Log_Click(object sender, RoutedEventArgs e)
+        {
+            var path = Directory.GetCurrentDirectory() + AppSettings.GetLogPath();
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            Process.Start(path);
+        }
+        private void Image_Click(object sender, RoutedEventArgs e)
+        {
+
+            var path = Directory.GetCurrentDirectory() + AppSettings.GetImagePath();
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            Process.Start(path);
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SetupTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Tick += timer_Tick;
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            SetupListBox();
+        }
+
+        private void SetupListBox()
+        {
+            var path = Directory.GetCurrentDirectory() + AppSettings.GetImagePath();
+            var imageFiles = ImageHandler.LoadImageFiles(path);
+
+            if (imageFiles == null) return;
+
+            var list = imageFiles.Select(imageFile => imageFile.Name).ToList();
+            ListBox.ItemsSource = list;
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (_imageFrame == null)
+            {
+                _imageFrame = new ImageFrame(ListBox.SelectedItem.ToString());
+                _imageFrame.Show();
+            }
+            else
+            {
+                _imageFrame.LoadImage(ListBox.SelectedItem.ToString());
+                _imageFrame.Show();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_imageFrame == null) return;
+            _imageFrame.CloseImage();
         }
     }
 }
